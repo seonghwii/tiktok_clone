fe229083f1a19c18a6cf1275b525d6a0568463d2
seonghwi <shlee@fittingmonster.com>
2023-03-16 14:02:45 +0900

    onboarding 완료(5.x)

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -1,6 +1,27 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tictok_clone/constants/gaps.dart';
 import 'package:tictok_clone/constants/sizes.dart';
+
+enum Direction { right, left }
+
+enum Page { first, second }
+
+/*Text(
+     "Watch cool videos!",
+    style: TextStyle(
+      fontSize: Sizes.size40,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+  Gaps.v16,
+  Text(
+    "Videos are personalized for you based on what you watch, like, and share.",
+    style: TextStyle(
+      fontSize: Sizes.size20,
+      color: Colors.grey.shade700,
+    ),
+  ), */
 
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
@@ -10,106 +31,111 @@ class TutorialScreen extends StatefulWidget {
 }
 
 class _TutorialScreenState extends State<TutorialScreen> {
+  Direction _direction = Direction.right;
+  Page _showingPage = Page.first;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    if (details.delta.dx > 0) {
+      // to the right
+      setState(() {
+        _direction = Direction.right;
+      });
+    } else {
+      // to the left
+      _direction = Direction.left;
+    }
+  }
+
+  void _onPanEnd(DragEndDetails detail) {
+    if (_direction == Direction.left) {
+      setState(() {
+        _showingPage = Page.second;
+      });
+    } else {
+      setState(() {
+        _showingPage = Page.first;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate, // 화면의 동작 감지(드래그의 방향을 알아낼 수 있다.)
+      onPanEnd: _onPanEnd, // 동작이 멈췄을 때 시행할 동작 지정(화면 전환)
       child: Scaffold(
-        body: SafeArea(
-          child: TabBarView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, // 텍스트 정렬
-                  children: [
-                    Gaps.v52,
-                    const Text(
-                      "Watch cool videos!",
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v16,
-                    Text(
-                      "Videos are personalized for you based on what you watch, like, and share.",
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                        color: Colors.grey.shade700,
-                      ),
-                    ),
-                  ],
-                ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+          child: SafeArea(
+            child: AnimatedCrossFade(
+              firstChild: const TutorialPage(
+                title: "Watch cool videos!",
+                contents:
+                    "Videos are personalized for you based on what you watch, like, and share.",
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, // 텍스트 정렬
-                  children: [
-                    Gaps.v52,
-                    const Text(
-                      "Follow the rules of the app",
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v16,
-                    Text(
-                      "Videos are personalized for you based on what you watch, like, and share.",
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                        color: Colors.grey.shade700,
-                      ),
-                    ),
-                  ],
-                ),
+              secondChild: const TutorialPage(
+                title: "Follow the rules",
+                contents: "Take care of one another! Plis!",
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, // 텍스트 정렬
-                  children: [
-                    Gaps.v52,
-                    const Text(
-                      "Enjoy the ride",
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v16,
-                    Text(
-                      "Videos are personalized for you based on what you watch, like, and share.",
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                        color: Colors.grey.shade700,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              crossFadeState: _showingPage == Page.first
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: const Duration(milliseconds: 300),
+            ),
           ),
         ),
         bottomNavigationBar: BottomAppBar(
-          child: Container(
+          child: Padding(
             padding: const EdgeInsets.symmetric(
-              vertical: Sizes.size48,
-            ),
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TabPageSelector(
-                  color: Colors.white,
-                  selectedColor: Colors.black38,
-                ),
-              ],
+                vertical: Sizes.size24, horizontal: Sizes.size24),
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 300),
+              opacity: _showingPage == Page.first ? 0 : 1, // 첫번째 페이지에서는 버튼 숨김
+              child: CupertinoButton(
+                onPressed: () {},
+                color: Theme.of(context).primaryColor,
+                child: const Text('Enter the app!'),
+              ),
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class TutorialPage extends StatelessWidget {
+  const TutorialPage({
+    super.key,
+    required this.title,
+    required this.contents,
+  });
+
+  final String title;
+  final String contents;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Gaps.v80,
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: Sizes.size40,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Gaps.v16,
+        Text(
+          contents,
+          style: TextStyle(
+            fontSize: Sizes.size20,
+            color: Colors.grey.shade700,
+          ),
+        ),
+      ],
     );
   }
 }
